d20476180c9dd26b79cc4688e2b3b196a8090987
laureen169 <bridgetlaureen3@gmail.con>
2022-04-28 16:34:55 +0400

    Uploading the UberClone partly done Updating the read me

---
+++ app/src/main/java/com/example/mysignupapp/Clients.java
@@ -0,0 +1,20 @@
+package com.example.mysignupapp;
+
+public class Clients
+{
+    private String firstName;
+    private String secondName;
+    private String userName;
+    private String email;
+    private String phoneNumber;
+
+    public Clients( String fN, String sN, String uN, String  eM, String pN){
+        this.firstName = fN;
+        this.secondName = sN;
+        this.userName = uN;
+        this.email = eM;
+        this.phoneNumber = pN;
+    }
+
+
+}
